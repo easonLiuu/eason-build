137a5854613ed432332e2dc4b53cb8a29f3512a3
liujiarui <ljr3289595084@163.com>
2023-04-23 20:23:57 +0800

    feat: node版本校验&命令注册

---
+++ bin/eason-build.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+const commander = require('commander')
+const { program } = require('commander')
+const pkg = require('../package.json')
+const checkNode = require('../lib/checkNode')
+const startServer = require('../lib/start/startServer')
+const buildServer = require('../lib/build/buildServer')
+
+const MIN_NODE_VERSION = '8.9.0';
+
+(async () => {
+    try {
+        if (!checkNode(MIN_NODE_VERSION)) {
+            throw new Error('Please upgrade your node version to v' + MIN_NODE_VERSION)
+        }
+        // 拿到版本号
+        program.version(pkg.version)
+
+        program
+            .command('start')
+            .description('start server by eason-build ')
+            .allowUnknownOption()
+            .action(startServer)
+        program
+            .command('build')
+            .description('build project by eason-build')
+            .allowUnknownOption()
+            .action(buildServer)
+        program.parse(process.argv)
+    } catch (error) {
+        console.log(e.message)
+    }
+})()
