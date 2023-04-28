59189916ecd2b70ea4fe1e3bb007372c7dc31aad
liujiarui <ljr3289595084@163.com>
2023-04-28 17:01:53 +0800

    feat: 插件运行开发

--- samples/eason-config.mjs
+++ samples/eason-config.mjs
@@ -1,12 +1,25 @@
 export default {
   entry: "src/index.js",
-  plugins: ["eason-build-test"],
+  plugins: function() {
+    return [
+      ['eason-build-test', {a: 1, b: 2}],
+      function() {
+        console.log('this is anonymous plugin')
+      }
+    ]
+  },
   hooks: [
-    ["start", (context) => {
-        console.log('start', context)
-    }],
-    ["configResolved",  (context) => {
-        console.log('configResolved', context)
-    }],
+    [
+      "start",
+      (context) => {
+        console.log("start", context);
+      },
+    ],
+    [
+      "configResolved",
+      (context) => {
+        console.log("configResolved", context);
+      },
+    ],
   ],
 };
